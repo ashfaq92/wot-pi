56c0237dbefa741bb0d78f6fde61267334d934c7
ashfaq92 <ashfaq92@outlook.com>
2025-12-24 17:37:27 +0200

    test

--- wotServer.js
+++ wotServer.js
@@ -2,5 +2,5 @@ const httpServer = require('./servers/http')
 const resources = require('./resources/model')
 
 const server = httpServer.listen(resources.pi.port, function() {
-    console.info(`Your WoT Pi is up and running on port ${resources.pi.port}`)
+    console.info(`Behold! Your WoT Pi has awoken from its digital slumber on port ${resources.pi.port} 🐍💻`)
 })
